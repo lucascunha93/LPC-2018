134522541d1216e8a235d2bb29fd8b4052dafdaf
Lucas Cunha <you@example.com>
2018-08-31 16:03:20 -0300

    Atividades aula5 - EF

---
+++ Aula02/TesteMVC/AppHostingMvc/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AppHostingMvc.Models;
+
+namespace AppHostingMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            List <Gerente> gerentes = new List<Gerente>();
+
+            gerentes.Add(new Gerente{id = 1, nome = "Adri"});
+            gerentes.Add(new Gerente{id = 2, nome = "Ramon"});
+
+            return View(gerentes);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
