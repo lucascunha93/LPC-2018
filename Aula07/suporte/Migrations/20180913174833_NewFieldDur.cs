cdaa6afc8da592a295bde5fcdf3b099b861b1d39
Lucas Cunha <you@example.com>
2018-09-14 16:22:21 -0300

    Praticando MVC e EF aula 07

---
+++ Aula07/suporte/Migrations/20180913174833_NewFieldDur.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace chamados.Migrations
+{
+    public partial class NewFieldDur : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "data",
+                table: "Chamado");
+
+            migrationBuilder.DropColumn(
+                name: "solucaoData",
+                table: "Chamado");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "horaInicio",
+                table: "Chamado",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(TimeSpan));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "horaFim",
+                table: "Chamado",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(TimeSpan));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "horaInicio",
+                table: "Chamado",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "horaFim",
+                table: "Chamado",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "data",
+                table: "Chamado",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "solucaoData",
+                table: "Chamado",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
