dad65bdcc656f52195727e3470df73ce2586d2d4
Lucas Cunha <you@example.com>
2018-09-28 18:15:39 -0300

    Iteração chamados-people

---
+++ Aula8-Chamados-AppPeople/Iteracao/Migrations/20180922001025_InitialMigrations.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppChamados.Migrations
+{
+    public partial class InitialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(nullable: true),
+                    telefone = table.Column<string>(nullable: true),
+                    email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Situacoes",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Situacoes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chamados",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    dataChamado = table.Column<DateTime>(nullable: false),
+                    descricao = table.Column<string>(nullable: true),
+                    dataSolucao = table.Column<DateTime>(nullable: true),
+                    horaInicio = table.Column<TimeSpan>(nullable: false),
+                    horaFim = table.Column<TimeSpan>(nullable: true),
+                    duracaoChamado = table.Column<TimeSpan>(nullable: true),
+                    situacaoid = table.Column<int>(nullable: true),
+                    clienteid = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chamados", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Chamados_Clientes_clienteid",
+                        column: x => x.clienteid,
+                        principalTable: "Clientes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Chamados_Situacoes_situacaoid",
+                        column: x => x.situacaoid,
+                        principalTable: "Situacoes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_clienteid",
+                table: "Chamados",
+                column: "clienteid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_situacaoid",
+                table: "Chamados",
+                column: "situacaoid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chamados");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Situacoes");
+        }
+    }
+}
